c306d85c77be0f92e0e56f96eef16e2e4fd5c4ec
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-09-16 00:08:39 +0200

    validation with express-validator

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // dotenv loads parameters (port and database config) from .env
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const connection = require('./db');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./db");
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // respond to requests on `/api/users`
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   // send an SQL query to get all users
-  connection.query('SELECT * FROM user', (err, results) => {
+  connection.query("SELECT * FROM user", (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).json({
@@ -25,25 +26,32 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.post('/api/users', (req, res) => {
-  // send an SQL query to get all users
-  connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
-    if (err) {
-      // If an error has occurred, then the client is informed of the error
-      res.status(500).json({
-        error: err.message,
-        sql: err.sql,
-      });
-    } else {
-      // If everything went well, we send the result of the SQL query as JSON
-      res.json(results);
+app.post(
+  "/api/users",
+  [body("email").isEmail(), body("password").isLength({ min: 8 })],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  });
-});
+    connection.query("INSERT INTO user SET ?", req.body, (err, results) => {
+      if (err) {
+        // If an error has occurred, then the client is informed of the error
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      } else {
+        // If everything went well, we send the result of the SQL query as JSON
+        res.json(results);
+      }
+    });
+  }
+);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
-    throw new Error('Something bad happened...');
+    throw new Error("Something bad happened...");
   }
 
   console.log(`Server is listening on ${process.env.PORT}`);
