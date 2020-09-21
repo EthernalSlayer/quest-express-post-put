82519220da3da2d8c8b2efcb54a4de70a78dcffd
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-09-21 10:54:45 +0200

    solution for the response express validator

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // dotenv loads parameters (port and database config) from .env
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const { check, validationResult } = require('express-validator');
-const connection = require('./db');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
+const connection = require("./db");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // respond to requests on `/api/users`
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   // send an SQL query to get all users
-  connection.query('SELECT * FROM user', (err, results) => {
+  connection.query("SELECT * FROM user", (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).json({
@@ -26,36 +26,53 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.post('/api/users', [
-  // email must be valid
-  check('email').isEmail(),
-  // password must be at least 8 chars long
-  check('password').isLength({ min: 8 }),
-  // let's assume a name should be 2 chars long
-  check('name').isLength({ min: 2 }),
-],
-(req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-  // send an SQL query to get all users
-  return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
-    if (err) {
-      // If an error has occurred, then the client is informed of the error
-      return res.status(500).json({
-        error: err.message,
-        sql: err.sql,
-      });
+app.post(
+  "/api/users",
+  [
+    // email must be valid
+    check("email").isEmail(),
+    // password must be at least 8 chars long
+    check("password").isLength({ min: 8 }),
+    // let's assume a name should be 2 chars long
+    check("name").isLength({ min: 2 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-    // If everything went well, we send the result of the SQL query as JSON
-    return res.json(results);
-  });
-});
+    // send an SQL query to get all users
+    return connection.query(
+      "INSERT INTO user SET ?",
+      req.body,
+      (err, results) => {
+        if (err) {
+          // If an error has occurred, then the client is informed of the error
+          return res.status(500).json({
+            error: err.message,
+            sql: err.sql,
+          });
+        }
+        // If everything went well, we send the result of the SQL query as JSON
+        return connection.query(
+          "SELECT name, email FROM user WHERE id = ?",
+          results.insertId,
+          (err2, records) => {
+            // console.log(records);
+            return res.status(201).json({
+              email: records[0].email,
+              name: records[0].name,
+            });
+          }
+        );
+      }
+    );
+  }
+);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
-    throw new Error('Something bad happened...');
+    throw new Error("Something bad happened...");
   }
 
   console.log(`Server is listening on ${process.env.PORT}`);
